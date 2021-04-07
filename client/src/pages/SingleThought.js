21dcbd6a0864704e22dad8d5db3a7c3eaaa382f1
mheyn95 <mheyn95@gmail.com>
2021-04-07 13:43:35 -0600

    added reactions

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -4,6 +4,9 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 
 import { QUERY_THOUGHT } from "../utils/queries";
+
+import Auth from "../utils/auth";
+import ReactionForm from "../components/ReactionForm";
 import ReactionList from "../components/ReactionList";
 
 const SingleThought = (props) => {
@@ -36,6 +39,7 @@ const SingleThought = (props) => {
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions} />
       )}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
